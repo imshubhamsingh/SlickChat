ddd428c22a3628567b61cf216ffb47684232e818
Shubham Singh <imshubhamsingh97@gmail.com>
2017-07-16 16:05:45 +0530

    beta phase

--- app/users/profile.controller.js
+++ app/users/profile.controller.js
@@ -2,15 +2,3 @@
  * Created by shubham on 8/7/17.
  */
 
-angular.module('SlickChatApp')
-    .controller('ProfileCtrl', function($state, md5, auth, profile){
-        var profileCtrl = this;
-        profileCtrl.profile = profile;
-
-        profileCtrl.updateProfile = function(){
-            profileCtrl.profile.emailHash = md5.createHash(auth.email);
-            profileCtrl.profile.$save();
-            $state.go('channels');
-        };
-
-    });
